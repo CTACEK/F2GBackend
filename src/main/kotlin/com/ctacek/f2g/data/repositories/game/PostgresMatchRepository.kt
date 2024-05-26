9c81f1a9870bea51c927978ce2730898db12f81c
ctacek <mrusboy@mail.ru>
2024-05-26 18:37:22 +0300

    Delete unused files + factoring + added film's request from kinopoisk api

--- src/main/kotlin/com/ctacek/f2g/data/repositories/game/PostgresMatchRepository.kt
+++ src/main/kotlin/com/ctacek/f2g/data/repositories/game/PostgresMatchRepository.kt
@@ -67,9 +67,7 @@ class PostgresMatchRepository(
     override suspend fun setGameState(roomId: String, state: Boolean): Boolean {
         val affectedRows = database.update(Rooms) {
             set(it.gameStarted, state)
-            where {
-                it.id eq roomId
-            }
+            where { it.id eq roomId }
         }
         return if (affectedRows == 1) {
             _updates.emit(UpdateModel.GameStateUpdate(roomId, state))
